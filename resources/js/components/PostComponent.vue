5e8eecfd9767b31fa92cb69622c0dd06d703cc59
AbuBakr-2000 <abdumannopovabubakir2000@mail.ru>
2022-05-29 12:49:47 +0500

    init laravel

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,16 @@
+<!-- <template>
+  <div>
+      11111111111111
+  </div>
+</template>
+
+<script>
+    export default {
+
+        name:"PostComponent"
+    }
+</script>
+
+<style scoped>
+    
+</style> -->
